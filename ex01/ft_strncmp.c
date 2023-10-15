7243f309bea285c2b5106dc810c4309af58cae11
Mue <Mue@mue.com>
2023-10-15 21:35:45 +0300

    fixed exercise 01

--- ex01/ft_strncmp.c
+++ ex01/ft_strncmp.c
@@ -16,12 +16,12 @@ int ft_strncmp(char *s1, char *s2, unsigned int n)
         {
             if(s1[i] < s2[i])
             {
-                return 1;
+                return -1;
             }
 
             if(s1[i] > s2[i])
             {
-                return -1;
+                return 1;
             }
         }
     }
